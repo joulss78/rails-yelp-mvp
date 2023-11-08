bb056a8e18d728514c7d896cedd928e63ed145cb
Julien Lesueur <julienlesueur@outlook.fr>
2023-11-08 16:21:40 +0900

    yelp mvp v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+bristol = {name: "Le Bristol", address: "112 Rue du Faubourg Saint-Honoré, 75008 Paris", category: "french"}
+opera_mandarin = {name: "Opera Mandarin", address: "23 Bd des Capucines, 75002 Paris", category: "chinese"}
+il_cilento = {name: "Il Cilento", address: "34 Av. de Saint-Cloud, 78000 Versailles", category: "italian"}
+maison_fuji = {name: "Maison Fuji", address: "79 Rue de la Paroisse, 78000 Versailles", category: "japanese"}
+leon = {name: "Leon", address: "131 Bd Saint-Germain, 75006 Paris", category: "belgian"}
+
+[bristol, opera_mandarin, il_cilento, maison_fuji, leon].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
